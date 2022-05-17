9c8e1663299e9db1a3d3da91202640d5ec8dbb08
Mickey327 <usp2002@mail.ru>
2022-05-17 11:17:23 +0300

    initial project commit

---
+++ src/main/java/com/example/confectioneryApp/ConfectioneryAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.confectioneryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfectioneryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfectioneryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/confectioneryApp/ConfectioneryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.confectioneryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfectioneryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
